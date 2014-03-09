d4b6afe8cce4f2ad66a233dab17cf50bd7adfd9d
Christopher Toth <q.alpha@gmail.com>
2014-03-09 17:55:49 -0600

    simple module to return various data about the host system including hardware, software, and interpreter information, requires psutil

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+ name = 'sysinfo',
+ version = 0.1,
+ description = """Return various data about the host system including hardware, software, and interpreter information""",
+ py_modules = ['sysinfo',],
+ install_requires=['psutil',],
+)
