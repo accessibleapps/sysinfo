c26a74934c6490f6280324090b0ffad6d9e1ceef
Christopher Toth <q.alpha@gmail.com>
2015-08-26 20:23:34 -0600

    Bumped version to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
  name = 'sysinfo',
- version = 0.1,
+ version = 0.2,
  description = """Return various data about the host system including hardware, software, and interpreter information""",
  py_modules = ['sysinfo',],
  install_requires=['psutil==2.2.1',],
