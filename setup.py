8e0b8d3ede945345411ada813c39094895022d8c
Christopher Toth <q.alpha@gmail.com>
2018-01-13 20:27:15 -0700

    remove psutil version pinning

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
  version = 0.2,
  description = """Return various data about the host system including hardware, software, and interpreter information""",
  py_modules = ['sysinfo',],
- install_requires=['psutil==2.2.1',],
+ install_requires=['psutil',],
 )
